c4bf09ef55cbbfda896c16d53ea33bd591c30a0d
Jeroen De Dauw <jeroendedauw@gmail.com>
2022-07-27 23:58:29 +0200

    Create WikibaseRdfIntegrationTest

--- tests/EntryPoints/MappingUiLoadingTest.php
+++ tests/EntryPoints/MappingUiLoadingTest.php
@@ -4,19 +4,15 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\WikibaseRDF\Tests;
 
-use Article;
 use ProfessionalWiki\WikibaseRDF\Application\Mapping;
 use ProfessionalWiki\WikibaseRDF\Application\MappingList;
-use ProfessionalWiki\WikibaseRDF\WikibaseRdfExtension;
-use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
-use Wikibase\Repo\WikibaseRepo;
 
 /**
  * @covers \ProfessionalWiki\WikibaseRDF\EntryPoints\MediaWikiHooks
  * @group Database
  */
-class MappingUiLoadingTest extends \MediaWikiIntegrationTestCase {
+class MappingUiLoadingTest extends WikibaseRdfIntegrationTest {
 
 	public function testExistingItemHasEditingUiWithItsMappings(): void {
 		$this->createItemWithMappings(
@@ -30,29 +26,6 @@ class MappingUiLoadingTest extends \MediaWikiIntegrationTestCase {
 		$this->assertStringContainsString( 'wikibase-rdf', $html );
 		$this->assertStringContainsString( 'wiki:hosting', $html );
 		$this->assertStringContainsString( 'https://pro.wiki', $html );
-	}
-
-	private function getPageHtml( string $pageTitle ): string {
-		$title = \Title::newFromText( $pageTitle );
-
-		$article = new Article( $title, 0 );
-		$article->getContext()->getOutput()->setTitle( $title );
-
-		$article->view();
-
-		return $article->getContext()->getOutput()->getHTML();
-	}
-
-	private function createItemWithMappings( ItemId $itemId, MappingList $mappingList ): void {
-		$user = self::getTestSysop()->getUser();
-
-		WikibaseRepo::getEntityStore()->saveEntity(
-			new Item( $itemId ),
-			'',
-			$user
-		);
-
-		WikibaseRdfExtension::getInstance()->newMappingRepository( $user )->setMappings( $itemId, $mappingList );
 	}
 
 	public function test404ItemHasNoEditingUI(): void {
