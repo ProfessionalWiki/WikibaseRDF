5fb8a29476fe7a5d187315aef81a92649be2e987
Morne Alberts <morne@malberts.net>
2022-07-28 14:51:16 +0200

    Fix JS dependency delay

--- resources/ext.wikibase.rdf.js
+++ resources/ext.wikibase.rdf.js
@@ -8,10 +8,16 @@ $( function () {
 	// Move Mappings before Statements section.
 	$( 'h2.wikibase-statements' ).before( $( '#wikibase-rdf' ) );
 
-	// TODO: toggler state needs to be remembered.
-	var toggler = $( '#wikibase-rdf-toggler' ).toggler( {
-		$subject: $( '#wikibase-rdf-mappings' ),
-		visible: false
-	} );
-	toggler.find( '.ui-toggler-label' ).text( mw.msg( 'wikibase-rdf-mappings-toggler' ) );
+	mw.loader.using(
+		'wikibase.view.ControllerViewFactory',
+		function () {
+			// TODO: toggler state needs to be remembered.
+			var toggler = $( '#wikibase-rdf-toggler' ).toggler( {
+				$subject: $( '#wikibase-rdf-mappings' ),
+				visible: false
+			} );
+			toggler.find( '.ui-toggler-label' ).text( mw.msg( 'wikibase-rdf-mappings-toggler' ) );
+		}
+	);
+
 } );
